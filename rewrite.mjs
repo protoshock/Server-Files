b3e7e5ee869b60425051a6235861c5080394531c
Gizzy <nerop455@gmail.com>
2023-10-26 22:06:46 +0100

    Ping Improve (Hopefully)

--- rewrite.mjs
+++ rewrite.mjs
@@ -3,38 +3,40 @@ import { Server } from 'socket.io';
 import express from 'express';
 import { uptime } from 'node:process';
 import { totalmem, freemem } from 'node:os';
-import crypto from "node:crypto";
+import crypto from 'node:crypto';
 import pkg from 'node-gzip';
 import fs from 'fs';
 const { gzip, ungzip } = pkg;
+
 const app = express();
 const server = createServer(app);
 app.use('/public', express.static('public'));
+
 app.get('/', (req, res) => {
-  let page = fs.readFileSync('./index.html', { encoding: 'utf-8' })
+  let page = fs.readFileSync('./index.html', { encoding: 'utf-8' });
   res.send(page);
-})
+});
 
 app.get('/:roomId', (req, res) => {
   let roomId = req.params.roomId;
   const room = serverData.rooms.get(roomId);
-  if(!room) return res.redirect('/');
-  let page = fs.readFileSync('./room.html', { encoding: 'utf-8' })
-  page = page.replace(/{room-name}/g, room.name)
-  page = page.replace(/{room-count}/g, room.playerCount)
-  page = page.replace(/{room-maxcount}/g, room.maxplayers)
-  page = page.replace(/{room-version}/g, room.gameversion)
+  if (!room) return res.redirect('/');
+  let page = fs.readFileSync('./room.html', { encoding: 'utf-8' });
+  page = page.replace(/{room-name}/g, room.name);
+  page = page.replace(/{room-count}/g, room.playerCount);
+  page = page.replace(/{room-maxcount}/g, room.maxplayers);
+  page = page.replace(/{room-version}/g, room.gameversion);
   res.send(page);
-})
+});
 
 let playerCount = 0;
 const serverData = {
-    players: new Map(),
-    rooms: new Map()
-}
+  players: new Map(),
+  rooms: new Map(),
+};
 
 function createId() {
-  return crypto.randomBytes(4).toString("hex");
+  return crypto.randomBytes(4).toString('hex');
 }
 
 function getPlayerBySocket(ws) {
@@ -64,14 +66,14 @@ function joinRoom(ws, roomId, _gameversion) {
     id: createId(),
     socket: ws,
     roomId: roomId,
-    local: true
+    local: true,
   };
   room.players.set(newPlayer.id, newPlayer);
   serverData.players.set(newPlayer.id, newPlayer);
   room.playerCount++;
-  playerCount++
-  console.log(`[Server] Player ${newPlayer.id} joined the room ${newPlayer.roomId}`)
-  console.log(`[Server] Room ${roomId}'s Player Count: ${room.playerCount}`)
+  playerCount++;
+  console.log(`[Server] Player ${newPlayer.id} joined the room ${newPlayer.roomId}`);
+  console.log(`[Server] Room ${roomId}'s Player Count: ${room.playerCount}`);
   console.log(`[Server] Global Player Count: ${playerCount}`);
   broadcastRoomInfo(ws);
 }
@@ -87,12 +89,12 @@ async function createRoom(ws, roomName, _scene, _scenepath, _gameversion, max) {
     name: roomName,
     gameversion: _gameversion,
     players: new Map(),
-    playerCount: '0'
+    playerCount: '0',
   };
   serverData.rooms.set(room.id, room);
   joinRoom(ws, room.id, _gameversion);
 }
-  
+
 function removePlayer(ws) {
   const player = getPlayerBySocket(ws);
   if (player == null) return;
@@ -101,8 +103,8 @@ function removePlayer(ws) {
     room.players.delete(player.id);
     room.playerCount--;
     playerCount--;
-    console.log(`[Server] Player ${player.id} left from the room ${player.roomId}`)
-    console.log(`[Server] Room ${player.roomId}'s Player Count: ${room.playerCount}`)
+    console.log(`[Server] Player ${player.id} left from the room ${player.roomId}`);
+    console.log(`[Server] Room ${player.roomId}'s Player Count: ${room.playerCount}`);
     console.log(`[Server] Global Player Count: ${playerCount}`);
     if (room.players.size === 0) {
       serverData.rooms.delete(room.id);
@@ -110,10 +112,10 @@ function removePlayer(ws) {
   }
   serverData.players.delete(player.id);
 }
-  
-setInterval(function(){
+
+setInterval(function () {
   global.gc();
-}, 1000*30);
+}, 1000 * 30);
 
 async function broadcastRoomInfo() {
   for (const [_, player] of serverData.players.entries()) {
@@ -123,7 +125,7 @@ async function broadcastRoomInfo() {
       const playerIds = Array.from(room.players.keys()).map((id) => ({
         playerId: id,
         local: id === player.id,
-        roomId: room.id
+        roomId: room.id,
       }));
       var json = JSON.stringify({
         action: 'roominfo',
@@ -131,7 +133,7 @@ async function broadcastRoomInfo() {
         scene: room.scene,
         scenepath: room.scenepath,
         gameversion: room.gameversion,
-        id: createId()
+        id: createId(),
       });
       SendMessage(player.socket, json);
     }
@@ -190,11 +192,11 @@ async function roomList(ws, amount, emptyonly) {
       break;
     }
     const _data = JSON.stringify({
-      action: "roomlist_roominfo",
+      action: 'roomlist_roominfo',
       roomName: room.name,
       roomId: room.id,
       roomversion: room.gameversion,
-      playercount: room.players.size
+      playercount: room.players.size,
     });
     if (room.players.size <= room.maxplayers || !emptyonly) {
       SendMessage(ws, _data);
@@ -213,7 +215,7 @@ async function getCurrentPlayers(ws) {
     const playerIds = Array.from(room.players.keys()).map((id) => ({
       playerId: id,
       local: id === player.id,
-      roomId: room.id
+      roomId: room.id,
     }));
     var json = JSON.stringify({
       action: 'currentplayers',
@@ -221,35 +223,35 @@ async function getCurrentPlayers(ws) {
       scene: room.scene,
       scenepath: room.scenepath,
       gameversion: room.gameversion,
-      id: createId()
+      id: createId(),
     });
     SendMessage(player.socket, json);
   }
 }
 
 function handleAction(ws, data) {
-    switch (data.action) {
-      case 'createRoom':
-        createRoom(ws, data.roomName, data.scene, data.scenepath, data.gameversion, data.maxplayers);
-        break;
-      case 'joinRoom':
-        joinRoom(ws, data.roomId, data.gameversion);
-        break;
-      case 'rpc':
-        handleRPC(ws, JSON.stringify(data));
-        break;
-      case 'getroomlist':
-        roomList(ws, data.amount, data.emptyonly);
-        break;
-      case 'getcurrentplayers':
-        getCurrentPlayers(ws);
-        break;
-      case 'leave':
-        removePlayer(ws);
-        break;
-      default:
-        break;
-    }
+  switch (data.action) {
+    case 'createRoom':
+      createRoom(ws, data.roomName, data.scene, data.scenepath, data.gameversion, data.maxplayers);
+      break;
+    case 'joinRoom':
+      joinRoom(ws, data.roomId, data.gameversion);
+      break;
+    case 'rpc':
+      handleRPC(ws, JSON.stringify(data));
+      break;
+    case 'getroomlist':
+      roomList(ws, data.amount, data.emptyonly);
+      break;
+    case 'getcurrentplayers':
+      getCurrentPlayers(ws);
+      break;
+    case 'leave':
+      removePlayer(ws);
+      break;
+    default:
+      break;
+  }
 }
 
 function SendMessage(ws, data) {
@@ -273,30 +275,30 @@ async function sendBundledCompressedMessages() {
   MessagesToSend.clear();
 }
 
-const wss = new Server(server, {
-    transports: ['websocket', 'polling'],
-    maxHttpBufferSize: 10e8,
-    pingTimeout: 60000
-});
-
 const connectedWebClients = new Map();
 const MessagesToSend = new Map();
 
 setInterval(sendBundledCompressedMessages, 1000 / 30);
 
+const wss = new Server(server, {
+  transports: ['websocket', 'polling'],
+  maxHttpBufferSize: 10e8,
+  pingTimeout: 60000
+});
+
 wss.on('connection', (ws) => {
   ws.on('message', async (message) => {
     const decompressedmessage = await ungzip(message);
     const buffer = Buffer.from(decompressedmessage);
-    const messagelist = buffer.toString('utf8').split("\n");
-    (messagelist).forEach(element => {
-        const data = JSON.parse(element);
-        handleAction(ws, data);
+    const messagelist = buffer.toString('utf8').split('\n');
+    messagelist.forEach((element) => {
+      const data = JSON.parse(element);
+      handleAction(ws, data);
     });
   });
 
-  ws.on("ping", (timestamp) => {
-    ws.volatile.emit("pong", timestamp);
+  ws.on('ping', (timestamp) => {
+    ws.volatile.emit('pong', timestamp);
     var player = getPlayerBySocket(ws);
     if (player != null) {
       player.lastMessageTime = Date.now();
@@ -308,43 +310,44 @@ wss.on('connection', (ws) => {
     console.log("[Server] Web Client Connected");
     setInterval(() => {
       if (connectedWebClients.size > 0) {
-          const roomslistid = [];
-          let count = 0;
-    
-          for (const [_, room] of serverData.rooms) {
-              if (count > 25) {
-                  break;
-              }
-              roomslistid.push({
-                  RoomID: room.id,
-                  RoomName: room.name,
-                  RoomPlayerCount: room.playerCount,
-                  RoomPlayerMax: room.maxplayers,
-                  RoomGameVersion: room.gameversion,
-              });
-              count++;
+        const roomslistid = [];
+        let count = 0;
+
+        for (const [_, room] of serverData.rooms) {
+          if (count > 25) {
+            break;
           }
-    
-          const totalMemoryInBytes = totalmem();
-          const totalMemoryInMB = (totalMemoryInBytes / (1024 ** 2)).toFixed(2);
-    
-          const freeMemoryInBytes = freemem();
-          const freeMemoryInMB = (freeMemoryInBytes / (1024 ** 2)).toFixed(2);
-    
-          const usedMemoryInMB = (totalMemoryInMB - freeMemoryInMB).toFixed(2);
-    
-          for (const client of connectedWebClients.values()) {
-              const data = {
-                  rooms: roomslistid,
-                  globalplayercount: playerCount,
-                  uptime: convertSecondsToUnits(Math.round(uptime())),
-                  usage: Math.round(usedMemoryInMB),
-              };
-              client.emit("webmessageclient", data);
-          }
+          roomslistid.push({
+            RoomID: room.id,
+            RoomName: room.name,
+            RoomPlayerCount: room.playerCount,
+            RoomPlayerMax: room.maxplayers,
+            RoomGameVersion: room.gameversion,
+          });
+          count++;
+        }
+
+        const totalMemoryInBytes = totalmem();
+        const totalMemoryInMB = (totalMemoryInBytes / (1024 ** 2)).toFixed(2);
+
+        const freeMemoryInBytes = freemem();
+        const freeMemoryInMB = (freeMemoryInBytes / (1024 ** 2)).toFixed(2);
+
+        const usedMemoryInMB = (totalMemoryInMB - freeMemoryInMB).toFixed(2);
+
+        for (const client of connectedWebClients.values()) {
+          const data = {
+            rooms: roomslistid,
+            globalplayercount: playerCount,
+            uptime: convertSecondsToUnits(Math.round(uptime())),
+            usage: Math.round(usedMemoryInMB),
+          };
+          client.emit("webmessageclient", data);
+        }
       }
     }, 1000);
   });
+
   ws.on('disconnect', () => {
     if (connectedWebClients.has(ws)) {
       connectedWebClients.delete(ws);
