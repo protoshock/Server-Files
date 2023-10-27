42ca7b8726702a5453c58b5a32714fee0f6a724f
Gizzy <nerop455@gmail.com>
2023-10-27 03:10:16 +0100

    entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,9 @@
 #!/bin/sh
-WORK_DIR="/usr/src/app"
-
-cd "$WORK_DIR"
-
-git fetch origin
-LOCAL=$(git rev-parse @)
-REMOTE=$(git rev-parse origin/main)
-
-if [ "$LOCAL" != "$REMOTE" ]; then
-  echo "New code available. Updating..."
-  git pull
-  node --expose-gc .
-  # Additional commands to restart your application if needed
-else
-  echo "No new updates found."
-  node --expose-gc .
-fi
+cd /usr/src
+rm -rf app
+echo "Getting Repo Code"
+git clone https://git.furgiz.eu.org/Gizzy/Edited-Protoshock-Server.git app
+cd app
+node --expose-gc .
 
 exec "$@"
