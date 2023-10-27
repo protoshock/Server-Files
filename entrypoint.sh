c192be7d0b847440c69105ea68c811ddd5604050
Gizzy <nerop455@gmail.com>
2023-10-27 03:10:56 +0100

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,6 @@ rm -rf app
 echo "Getting Repo Code"
 git clone https://git.furgiz.eu.org/Gizzy/Edited-Protoshock-Server.git app
 cd app
-node --expose-gc .
+node --expose-gc rewrite.mjs
 
 exec "$@"
